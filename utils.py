cd067156346be743f05322c4ba6a24d9cd7323c6
kshtiijrajsharma <skshitizraj@gmail.com>
2024-02-26 12:29:28 +0545

    Updated understanding the metrics section with unique users and total interaction metrics

--- utils.py
+++ utils.py
@@ -27,6 +27,30 @@ def check_env_vars(enable_email=False):
         raise ValueError("Missing environment variables: " + ", ".join(missing_vars))
 
 
+def _generate_understanding_metrics_section():
+
+    explanation_section = """<h2>Understanding the Metrics:</h2>
+<p><em>Our report includes several key metrics that provide insights into the usage patterns of our services. Here's a brief explanation of these metrics:</em></p>
+<p><strong>Folders:</strong></p>
+<ul>
+<li>TM: Tasking Manager exports</li>
+<li>default: Default exports generated usually from export tool / FMTM and fAIr general call</li>
+<li>ISO3/HDX: Country exports currently pushed to HDX</li>
+</ul>
+<p><strong>Key Metrics:</strong></p>
+<ul>
+<li><strong>Total Downloads:</strong> Represents the total number of files retrieved ('GET' operations) from the service. This metric helps in understanding the demand for data stored in our service.</li>
+<li><strong>Total Uploads/Updates:</strong> Counts the total number of files uploaded or modified ('PUT'/ 'POST' operations) . This metric gives an idea about how much new data is being generated from our service.</li>
+<li><strong>Total Downloaded/Uploaded Data:</strong> Measures the total amount of data transferred during download and upload operations. It's a key indicator of the bandwidth utilized by these activities.</li>
+<li><strong>Unique Users:</strong> Indicates the total number of distinct users that have interacted with our service, based on unique IP addresses. This helps in understanding the reach of our service. It is possible to have multiple downloads from same ip, Specially if downloads are being redirected using some server it might record only one ip of server</li>
+<li><strong>Total User Interactions:</strong> The total number of interactions users have had with the service, excluding data upload/modification actions. This provides insight into how actively users are engaging with the service.</li>
+<li><strong>Most Popular Files by Interactions:</strong> Highlights the files with the highest number of accesses, indicating user interest or demand for specific data.</li>
+</ul>
+<p><strong>Note:</strong> Interactions refer to any action taken by users related to a file, including but not limited to downloading. For eg : User only listing the resource or fetching the file size etc</p>
+"""
+    return explanation_section
+
+
 def generate_full_report_email(df, presigned_url_csv):
     if not isinstance(df, pd.DataFrame):
         df = df.to_pandas()
@@ -39,6 +63,10 @@ def generate_full_report_email(df, presigned_url_csv):
     df["top_level_key"] = df["key"].apply(lambda x: x.split("/")[0])
     df["top_level_key"] = df["top_level_key"].replace("-", "default")
 
+    unique_users = df["remoteip"].nunique()
+
+    total_user_interactions = df[~df["method"].isin(["PUT", "POST", "DELETE"])].shape[0]
+
     total_downloads = df[df["method"] == "GET"]["objectsize"].count()
     total_uploads = df[df["method"] == "PUT"]["objectsize"].count()
     total_download_size_bytes = df[df["method"] == "GET"]["objectsize"].sum()
@@ -47,12 +75,14 @@ def generate_full_report_email(df, presigned_url_csv):
     timeframe_end = df["requestdatetime"].max().strftime("%B %d, %Y")
 
     def format_size(size_bytes):
-        if size_bytes < 1024:
+        if size_bytes == 0:
+            return "0 MB"
+        elif size_bytes < 1024:
             return "less than 1 MB"
         elif size_bytes < 1024**2:
             return f"{size_bytes / 1024:.0f} MB"
         else:
-            return f"{size_bytes / (1024**3):.0f} GB"
+            return f"{size_bytes / (1024**3):.2f} GB"
 
     email_body = f"""
 <html>
@@ -79,14 +109,11 @@ def generate_full_report_email(df, presigned_url_csv):
 <ul>
   <li>Total Downloads: {total_downloads}</li>
   <li>Total Uploads/Updates: {total_uploads}</li>
-  <li>Total Download Transferred: {format_size(total_download_size_bytes)}</li>
-  <li>Total Upload/Update Transferred: {format_size(total_upload_size_bytes)}</li>
+  <li>Total Downloaded Data: {format_size(total_download_size_bytes)}</li>
+  <li>Total Uploaded/Updated Data: {format_size(total_upload_size_bytes)}</li>
+  <li>Unique Users: {unique_users}</li>
+  <li>Total User Interactions: {total_user_interactions}</li>
 </ul>
-<h2>Detailed Folder Statistics:</h2>
-<p><em>Folder Explanation:</em><br>
-- TM: Tasking Manager exports<br>
-- default: Default exports generated usually from export tool / FMTM and fAIr general call<br>
-- ISO3: Country exports currently pushed to HDX</p>
 """
 
     for folder in df["top_level_key"].unique():
@@ -101,16 +128,27 @@ def generate_full_report_email(df, presigned_url_csv):
         ].sum()
         upload_size_bytes = folder_df[folder_df["method"] == "PUT"]["objectsize"].sum()
 
-        popular_files = folder_df["key"].value_counts().head(5)
+        unique_users_folder = folder_df["remoteip"].nunique()
+        total_interactions_folder = folder_df[
+            ~folder_df["method"].isin(["PUT", "POST", "DELETE"])
+        ].shape[0]
+
+        popular_files = (
+            folder_df[~folder_df["method"].isin(["PUT", "POST", "DELETE"])]["key"]
+            .value_counts()
+            .head(5)
+        )
         email_body += f"""
 <h3>Folder: {folder}</h3>
 <ul>
   <li>Total Downloads: {downloads}</li>
   <li>Total Uploads/Updates: {uploads}</li>
-  <li>Total Download Transferred: {format_size(download_size_bytes)}</li>
-  <li>Total Upload/Update Transferred: {format_size(upload_size_bytes)}</li>
+  <li>Total Downloaded Data: {format_size(download_size_bytes)}</li>
+  <li>Total Uploaded/Updated Data: {format_size(upload_size_bytes)}</li>
+  <li>Unique Users: {unique_users_folder}</li>
+  <li>Total User Interactions: {total_interactions_folder}</li>
 </ul>
-<p><strong>Most Popular Files:</strong></p>
+<p><strong>Most Popular Files by Interactions:</strong></p>
 <ul>
 """
         for file, count in popular_files.items():
@@ -118,13 +156,18 @@ def generate_full_report_email(df, presigned_url_csv):
         email_body += "</ul>"
 
         if folder in ["TM", "ISO3"]:
-            folder_df["project"] = folder_df["key"].apply(
+            # Filter folder_df to exclude PUT, POST, and DELETE methods
+            filtered_df = folder_df[
+                ~folder_df["method"].isin(["PUT", "POST", "DELETE"])
+            ]
+
+            filtered_df["project"] = filtered_df["key"].apply(
                 lambda x: x.split("/")[1] if len(x.split("/")) > 1 else "NA"
             )
-            folder_df["feature"] = folder_df["key"].apply(
+            filtered_df["feature"] = filtered_df["key"].apply(
                 lambda x: x.split("/")[2] if len(x.split("/")) > 2 else "NA"
             )
-            folder_df["fileformat"] = folder_df["key"].apply(
+            filtered_df["fileformat"] = filtered_df["key"].apply(
                 lambda x: (
                     "Other"
                     if "/" in (x.split("_")[-1].split(".")[0])
@@ -132,15 +175,15 @@ def generate_full_report_email(df, presigned_url_csv):
                 )
             )
 
-            project_counts = folder_df["project"].value_counts().head(5)
-            feature_counts = folder_df["feature"].value_counts().head(5)
-            file_format_counts = folder_df["fileformat"].value_counts().head(5)
+            project_counts = filtered_df["project"].value_counts().head(5)
+            feature_counts = filtered_df["feature"].value_counts().head(5)
+            file_format_counts = filtered_df["fileformat"].value_counts().head(5)
 
             additional_stats = f"""
-<p><strong>Additional Stats for {folder}:</strong></p>
-<p><strong>Most Popular Projects:</strong></p>
-<ul>
-"""
+        <p><strong>Additional Stats for {folder}:</strong></p>
+        <p><strong>Most Popular Projects:</strong></p>
+        <ul>
+        """
             for project, count in project_counts.items():
                 additional_stats += f"   <li>{project}: {count} times</li>\n"
             additional_stats += "</ul>"
@@ -157,13 +200,16 @@ def generate_full_report_email(df, presigned_url_csv):
 
             email_body += additional_stats
 
+    email_body += _generate_understanding_metrics_section()
+
     email_body += f"""
-    <p>
-    Download the full report meta CSV for your custom analysis from the attached link.
-    Note: This link expires in 1 week so if you have any other queries, please contact the administrator or reply to this email.
+<hr style="border: 1px solid #ccc; margin-top: 20px;">
+<p style="font-size: 0.8em; color: #666;">
+    This email is auto-generated by <a href="https://github.com/hotosm/s3-logs-analyzer/" style="color: #666;">s3 logs analyzer</a> and might contain confidential data. You can download the full report meta CSV for your custom analysis from the attached link bearing in mind the link expires in 1 week. If you have any other queries, please contact the administrator or reply to this email.
     <br>
-    <a href="{presigned_url_csv}">Download Full Report</a>
+    <a href="{presigned_url_csv}" style="color: #666;">Download Full Data</a>
 </p>
+
 </body>
 </html>
 """
